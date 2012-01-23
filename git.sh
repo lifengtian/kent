d26b44a1c3342c4e090fc2af71bc4502475d4254
Lifeng Tian <lifeng4209@gmail.com>
2012-01-23 13:14:01 -0500

    add blat

---
+++ git.sh
@@ -0,0 +1,6 @@
+git config --global user.name "Lifeng Tian"
+git config --global user.email lifeng4209@gmail.com
+
+
+git remote add origin git@github.com:lifengtian/kent.git
+git push -u origin master
